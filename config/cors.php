170e2c1de929f2c1f0272d0ab0862d46658a81b4
jhruiz <jaiber.ruiz@correounivalle.edu.co>
2026-05-13 00:58:06 -0500

    ajuste de cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [],
+    'allowed_origins' => ['https://dayilu.miggoshop.com'],
 
     'allowed_origins_patterns' => ['#^https?://.*\.miggoshop\.com$#'],
 
