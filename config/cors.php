5e1af58be706a97f6634c7446789cf11a537fe89
jhruiz <jaiber.ruiz@correounivalle.edu.co>
2026-05-13 00:29:31 -0500

    se ajusta archivo de cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://*.miggo.com.co', 'https://*.miggoshop.com',],
+    'allowed_origins' => ['https://dayilu.miggoshop.com',],
 
     'allowed_origins_patterns' => [],
 
