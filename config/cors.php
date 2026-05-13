84bb3774c06033fc1a2238687c3e28409f7203a0
jhruiz <jaiber.ruiz@correounivalle.edu.co>
2026-05-13 00:10:22 -0500

    se agrega configuración de cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://pruebaecommerce.miggo.com.co'],
+    'allowed_origins' => ['https://*.miggo.com.co', 'https://*.miggoshop.com',],
 
     'allowed_origins_patterns' => [],
 
